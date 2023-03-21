50102e6760ed026b5f724263496b11252e803ffa
openwpdotorg <openwp.org@gmail.com>
2023-03-22 00:02:01 +0700

    init api

---
+++ api/database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'tranthethang',
+            'email' => 'tranthethang@gmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
+    }
+}
